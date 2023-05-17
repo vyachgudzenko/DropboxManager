4428d30bf095774eecdf2a92630562bd88e70b1e
vyachgudzenko <vyach.gudzenko@gmail.com>
2023-05-17 20:18:30 +0300

    Initial commit

---
+++ DropboxManager/App/DropboxManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropboxManagerApp.swift
+//  DropboxManager
+//
+//  Created by Вячеслав Гудзенко on 17.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct DropboxManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
